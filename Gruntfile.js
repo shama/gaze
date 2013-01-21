b4df6b3e8dfa7863c91ce2b6fed459ca89c890db
Kyle Robinson Young <kyle@dontkry.com>
2013-01-21 10:17:57 -0800

    Update deps. Remove watch task as dep of itself doesnt work.

--- Gruntfile.js
+++ Gruntfile.js
@@ -23,25 +23,10 @@ module.exports = function(grunt) {
       test: {
         src: ['test/**/*_test.js']
       },
-    },
-    watch: {
-      gruntfile: {
-        files: '<%= jshint.gruntfile.src %>',
-        tasks: ['jshint:gruntfile']
-      },
-      lib: {
-        files: '<%= jshint.lib.src %>',
-        tasks: ['jshint:lib', 'nodeunit']
-      },
-      test: {
-        files: '<%= jshint.test.src %>',
-        tasks: ['jshint:test', 'nodeunit']
-      },
-    },
+    }
   });
   grunt.loadNpmTasks('grunt-benchmark');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-nodeunit');
-  grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.registerTask('default', ['jshint', 'nodeunit']);
 };
