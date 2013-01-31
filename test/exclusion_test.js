fd0da0ecce99c1db8bfc39f0695eb2524fc46cd2
Jamie Stackhouse <contin673@gmail.com>
2013-01-30 23:31:14 -0400

    Code Review mchoy. Ran local tests, 42 passes. Yay. Tested locally as well using my project, and it worked the way I expected it too. Yay!

--- test/exclusion_test.js
+++ test/exclusion_test.js
@@ -7,6 +7,10 @@ exports.exclusion = {
     process.chdir(path.resolve(__dirname, 'fixtures'));
     done();
   },
+  tearDown: function(done) {
+    fs.unlinkSync(path.resolve(__dirname, 'fixtures', 'two.txt'));
+    done();
+  },
   exclusionTest: function(test) {
     var expected = {
       'Project (LO)/': ['one.js'],
@@ -22,14 +26,17 @@ exports.exclusion = {
         test.done();
       });
       //Write a file that shouldn't ever match a pattern, but is in the same directory as one that does.
+      console.log('Write TXT');
       fs.writeFileSync(path.resolve(__dirname, 'fixtures', 'two.txt'), 'Will I be watched?');
 
       //Write a file that is part of the exclusion.
+      console.log('Write Excluded File');
       fs.writeFileSync(path.resolve(__dirname, 'fixtures', 'nested', 'sub', 'two.js'), 'var two = true;');
 
       //Give time for watcher to respond and see if it responds to either file that shouldn't be watched.
       setTimeout(function() {
-        //fs.writeFileSync(path.resolve(__dirname, 'fixtures', 'sub', 'one.js'), 'var one = true;');
+        console.log('WATCH ME!');
+        fs.writeFileSync(path.resolve(__dirname, 'fixtures', 'sub', 'one.js'), 'var one = true;');
       }, 2000);
     })
   }
