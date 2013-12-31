c4fbfcf29c98627a6a0005508dfbd98a93971f50
Kyle Robinson Young <kyle@dontkry.com>
2013-12-31 14:32:58 -0800

    Add flatToTree helper for converting to old style tree

---
+++ test/helper_test.js
@@ -0,0 +1,55 @@
+'use strict';
+
+var helper = require('../lib/helper.js');
+var globule = require('globule');
+
+exports.helper = {
+  setUp: function(done) {
+    done();
+  },
+  tearDown: function(done) {
+    done();
+  },
+  flatToTree: function(test) {
+    test.expect(1);
+    var cwd = '/Users/dude/www/';
+    var files = [
+      '/Users/dude/www/',
+      '/Users/dude/www/one.js',
+      '/Users/dude/www/two.js',
+      '/Users/dude/www/sub/',
+      '/Users/dude/www/sub/one.js',
+      '/Users/dude/www/sub/nested/',
+      '/Users/dude/www/sub/nested/one.js',
+    ];
+    var expected = {
+      '/Users/dude/www/': ['/Users/dude/www/one.js', '/Users/dude/www/two.js', '/Users/dude/www/sub/'],
+      '/Users/dude/www/sub/': ['/Users/dude/www/sub/one.js', '/Users/dude/www/sub/nested/'],
+      '/Users/dude/www/sub/nested/': ['/Users/dude/www/sub/nested/one.js'],
+    };
+    var actual = helper.flatToTree(files, cwd);
+    test.deepEqual(actual, expected);
+    test.done();
+  },
+  flatToTreeRelative: function(test) {
+    test.expect(1);
+    var cwd = '/Users/dude/www/';
+    var files = [
+      '/Users/dude/www/',
+      '/Users/dude/www/one.js',
+      '/Users/dude/www/two.js',
+      '/Users/dude/www/sub/',
+      '/Users/dude/www/sub/one.js',
+      '/Users/dude/www/sub/nested/',
+      '/Users/dude/www/sub/nested/one.js',
+    ];
+    var expected = {
+      '.': ['one.js', 'two.js', 'sub/'],
+      'sub/': ['one.js', 'nested/'],
+      'sub/nested/': ['one.js'],
+    };
+    var actual = helper.flatToTree(files, cwd, true);
+    test.deepEqual(actual, expected);
+    test.done();
+  },
+};
