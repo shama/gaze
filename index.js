b344f5878bc84b3776fb80797840c1d41d655ce6
Kyle Robinson Young <kyle@dontkry.com>
2014-01-21 19:33:29 -0800

    If pathwatcher failed to install, just use gaze04. Closes GH-62.

--- index.js
+++ index.js
@@ -6,7 +6,14 @@
  * Licensed under the MIT license.
  */
 
-var version = process.versions.node.split('.');
-module.exports = (version[0] === '0' && version[1] === '8')
-  ? require('./lib/gaze04.js')
-  : require('./lib/gaze.js');
+try {
+  // Attempt to resolve optional pathwatcher
+  require.resolve('pathwatcher')
+  var version = process.versions.node.split('.');
+  module.exports = (version[0] === '0' && version[1] === '8')
+    ? require('./lib/gaze04.js')
+    : require('./lib/gaze.js');
+} catch (err)  {
+  // Otherwise serve gaze04
+  module.exports = require('./lib/gaze04.js');
+}
