6ca69ab559e8d54ba288ba59926460356a834b5e
Kyle Robinson Young <kyle@dontkry.com>
2014-03-21 15:36:28 -0700

    Add changed benchmark

---
+++ benchmarks/changed.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var gaze = require('../');
+var path = require('path');
+var fs = require('fs');
+var Benchmarker = require('./benchmarker');
+
+var b = new Benchmarker({ name: path.basename(__filename) });
+b.table.setHeading('files', 'ms').setAlign(0, 2).setAlign(1, 2);
+b.run(function(num, done) {
+  gaze('**/*', {cwd: b.tmpDir, maxListeners:0}, function(err, watcher) {
+    watcher.on('changed', function() {
+      b.log(num, b.end());
+      watcher.close();
+    });
+    watcher.on('end', done);
+    var randFile = path.join(b.tmpDir, 'test-' + Math.floor(Math.random() * num) + '.txt');
+    b.start();
+    fs.writeFileSync(randFile, '1234');
+  });
+}, function() {
+  process.exit();
+});
