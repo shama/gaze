aa494d0657af96337168c61c8a57f0229ae8d440
Kyle Robinson Young <kyle@dontkry.com>
2014-05-29 14:35:37 -0700

    Start on providing pre-built binaries

--- index.js
+++ index.js
@@ -6,17 +6,19 @@
  * Licensed under the MIT license.
  */
 
+var path = require('path');
+var fs = require('fs');
+
 // If on node v0.8, serve gaze04
 var version = process.versions.node.split('.');
 if (version[0] === '0' && version[1] === '8') {
   module.exports = require('./lib/gaze04.js');
 } else {
-  try {
-    // Check whether pathwatcher successfully built without running it
-    require('bindings')({ bindings: 'pathwatcher.node', path: true });
-    // If successfully built, give the better version
+  var v8 = 'v8-' + /[0-9]+\.[0-9]+/.exec(process.versions.v8)[0];
+  var pathwatcherPath = path.join(__dirname, 'bin', process.platform + '-' + process.arch + '-' + v8, 'pathwatcher.node');
+  if (fs.existsSync(pathwatcherPath)) {
     module.exports = require('./lib/gaze.js');
-  } catch (err) {
+  } else {
     // Otherwise serve gaze04
     module.exports = require('./lib/gaze04.js');
   }
